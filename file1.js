7685003e4610eb82ecb386e83d5e53e71e120b18
Bhavika Tawde <bhavikatawde0115@gmail.com>
2025-09-04 23:28:03 +0530

    Initial commit

---
+++ file1.js
@@ -0,0 +1,25 @@
+const http=require('http')
+const fs=require('fs');
+const server= http.createServer((req,res)=>{
+if(req.url === '/home'){
+fs.readFile('./home.html',(err, data)=>{
+if(err){
+res.writeHead(500,{'Content-Type': 'text/plain'});
+res.end('Error loading file');
+}
+else{
+res.writeHead(200,{'Content-Type': 'text/html'});
+res.end(data);
+}
+});
+}
+else{
+res.writeHead(400,{'Content-Type': 'text/html'});
+res.end('Page not found');
+}
+});
+server.listen(3000, () =>{
+console.log('Server is running at http://localhost:3000');
+});
+
+
